dc8de39bdd227354ea5df9ca433716052eaf565a
Flames <vibe-coder@flames.com>
2025-10-27 16:00:33 +0000

    Update project prj_u524v8h6 - 2025-10-27T16:00:32.535Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import RoutesWrapper from './components/RoutesWrapper';
 import Footer from './components/Footer';
@@ -7,11 +6,9 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-black text-white">
-      <BrowserRouter>
-        <Header />
-        <RoutesWrapper />
-        <Footer />
-      </BrowserRouter>
+      <Header />
+      <RoutesWrapper />
+      <Footer />
     </div>
   );
 }
